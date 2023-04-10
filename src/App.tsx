7e82c6a199a00d0462fc4b2a5ed46be59ad7f7d1
mrhiroman <sreutin@mail.ru>
2023-04-10 20:11:22 +0300

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter, Router, Routes} from 'react-router-dom'
 
 import { Header } from './components/layout/Header';
+import { Footer } from 'components/layout/Footer';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
       <Header />
+      <Footer />
     </div>
     </BrowserRouter>
   );
---
+++ src/components/layout/Footer/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import styles from './Footer.module.sass'
+
+import {Link} from 'react-router-dom'
+
+import Logo from 'assets/img/Logo.svg'
+import Payments from 'assets/img/footer/Payments.svg'
+
+export const Footer = () => {
+  return (
+    <div className={styles.layout}>
+        <div className={styles.footerUpper}>
+            <div>
+                <div className={styles.logoContainer}>
+                    <div className={styles.logo}>
+                        <img src={Logo} alt='logo' />
+                    </div>
+                    <div>YourWear</div>
+                </div>
+                <div className={styles.info}>sales@yourwear.com</div>
+                <div className={styles.info}>88005553535</div>
+            </div>
+            <div className={styles.column}>
+                <div className={styles.category}>Shop</div>
+                <Link to="">Design a Shirt</Link>
+                <Link to="">All goods</Link>
+                <Link to="">My orders</Link>
+            </div>
+            <div className={styles.column}>
+                <div className={styles.category}>About Us</div>
+                <Link to="">About Page</Link>
+                <Link to="">Terms and Conditions</Link>
+                <Link to="">Privacy Policy</Link>
+            </div>
+        </div>
+        <div className={styles.footerLower}>
+            <div className={styles.copyright}>© Copyright 2023 | All Rights Reserved By YourWear</div>
+            <div className={styles.payments}>
+                <img src={Payments} alt='payments' />
+            </div>
+        </div>
+    </div>
+  )
+}
+
