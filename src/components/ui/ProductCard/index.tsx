687c2e29eec3f51004b9f170adcf8f057116bb84
mrhiroman <sreutin@mail.ru>
2023-04-19 10:41:53 +0300

    card overlay + new scrollbar

--- src/components/ui/ProductCard/index.tsx
+++ src/components/ui/ProductCard/index.tsx
@@ -3,6 +3,10 @@ import styles from './ProductCard.module.sass'
 
 import DefaultShirt from 'assets/img/mocks/DefaultShirt.png'
 
+import HeartIcon from 'assets/img/pages/main/icons/Heart.svg'
+import BagIcon from 'assets/img/pages/main/icons/Bag.svg'
+import { Button, ButtonType } from '../Button'
+
 //add currency from global state(?)
 export type Product = {
     name: string, 
@@ -16,6 +20,15 @@ export const ProductCard = (props: {product: Product}) => {
     <div className={styles.card}>
         <div className={styles.image}>
             <img src={props.product.imageUrl !== undefined ? props.product.imageUrl : DefaultShirt} alt='product' />
+            <div className={styles.overlay}>
+              <div className={styles.circleButton}>
+                <img src={BagIcon} alt='buy' />
+              </div>
+              <Button type={ButtonType.Yellow} text='Customize' />
+              <div className={styles.circleButton}>
+                <img src={HeartIcon} alt='fav' />
+              </div>
+            </div>
         </div>
         <div className={styles.cardInfo}>
             <div className={styles.name}>{props.product.name}</div>
